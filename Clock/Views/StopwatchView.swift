4d49331a3bbbfc59536ec6312876e3c1ebcd19dc
Neil Shah <nshah@lcs.on.ca>
2024-12-06 14:33:51 -0500

    Added 3 views

---
+++ Clock/Views/StopwatchView.swift
@@ -0,0 +1,18 @@
+//
+//  StopwacthView.swift
+//  Clock
+//
+//  Created by Neil Shah on 2024-12-06.
+//
+
+import SwiftUI
+
+struct StopwatchView: View {
+    var body: some View {
+        Text("Stopwatch")
+    }
+}
+
+#Preview {
+    StopwatchView()
+}
---
+++ Clock/Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by Neil Shah on 2024-12-06.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text("World Clock")
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
