2db6a4eb0c8725523189d13660ac81a369b901f3
Neil Shah <nshah@lcs.on.ca>
2024-12-06 14:52:33 -0500

    Finished one row of the world clock row interface.

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -10,29 +10,52 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack {
-            Text("World Clock")
-                .navigationTitle("World Clock")
-                .toolbar {
-                    
-                    ToolbarItem(placement: .topBarLeading) {
-                        
-                        Button("Edit") {
-                            //Does nothing right now
-                        }
-                        
+            VStack{
+                //Ottawa
+                HStack {
+                    //Left side
+                    VStack {
+                        Text("Today, +0HRS")
+                        Text("Ottawa")
+                            .font(.system(.largeTitle, design: .default, weight: .thin))
                     }
+                    Spacer()
                     
-                    ToolbarItem(placement: .primaryAction) {
-                        Button {
-                            //Does nothing right now
-                        } label: {
-                            Image(systemName: "plus")
-                        }
-                    }
+                    //Right side
+                    Text("6:35")
+                        .font(.system(size: 64.0, weight: .thin, design: .default))
+                    Text("AM")
+                        .font(.system(.largeTitle, design: .default, weight: .thin))
+                    
+                    
                 }
+            }
+            .navigationTitle("World Clock")
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    
+                    Button("Edit") {
+                        //Does nothing right now
+                    }
+                    
+                }
+                
+                ToolbarItem(placement: .primaryAction) {
+                    
+                    Button {
+                        //Does nothing right now
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    
+                }
+                
+            }
         }
     }
 }
+
 #Preview {
     LandingView()
 }
