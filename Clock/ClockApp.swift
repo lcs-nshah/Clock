a81684fd2a12c30f0d7106a8c1ca43970978a339
Neil Shah <nshah@lcs.on.ca>
2024-12-06 14:22:25 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Neil Shah on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
