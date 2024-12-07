e99741892a84e1a32227885a3ed9b5e9054fb5a3
Neil Shah <nshah@lcs.on.ca>
2024-12-07 10:37:18 -0500

    Added toolbar and first alarm.

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -9,10 +9,51 @@ import SwiftUI
 
 struct AlarmsView: View {
     var body: some View {
-        Text("Alarms")
+        NavigationStack {
+            VStack {
+                HStack {
+                    VStack (alignment: .leading){
+                        HStack {
+                            //Left side
+                            Text("7:30")
+                                .font(.system(size: 64.0, weight: .thin, design: .default))
+                            Text("AM")
+                                .font(.system(.largeTitle, design: .default, weight: .thin))
+                        }
+                        Text("Alarm")
+                    }
+                    
+                    Spacer()
+                    
+                    //Right side
+                    Toggle("", isOn: Binding.constant(true))
+                        .tint(.green)
+                    
+                }
+                .padding()
+            }
+            .navigationTitle("Alarms")
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Edit") {
+                        //Does nothing right now
+                    }
+                }
+                ToolbarItem(placement: .primaryAction) {
+                    
+                    Button {
+                        //Does nothing right now
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    
+                }
+            }
+        }
     }
 }
-
-#Preview {
-    AlarmsView()
-}
+    
+    #Preview {
+        LandingView()
+    }
