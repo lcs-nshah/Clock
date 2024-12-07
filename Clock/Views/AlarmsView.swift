5d36ec6d2b0875f7fe7cb46282f1cfa33ef6aa2c
Neil Shah <nshah@lcs.on.ca>
2024-12-07 17:00:54 -0500

    Added sleep schedule and button for set up.

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -15,13 +15,29 @@ struct AlarmsView: View {
                     Text(Image(systemName: "bed.double.fill"))
                     + Text(" Sleep | Wake Up").font(.system(.title3, design: .default, weight: .bold))
                 }
-                .padding()
-                
+                .padding(.horizontal)
+                List {
+                    //Sleep schedule
+                   
+                    HStack {
+                        //Left side
+                        Text("No Alarm")
+                            .foregroundStyle(Color.gray)
+                        
+                        Spacer()
+                        
+                        //Right side
+                        Button {} label: {
+                            Text("SET UP")
+                                .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
+                                .foregroundStyle(.orange)
+                                .background(Color.gray, in: Capsule())
+                        }
+                        
+                    }
                     Text("Other")
                         .font(.system(.title2, design: .default, weight: .bold))
-                        .padding(.horizontal)
-                
-                List {
+                    
                     AlarmView(time: "7:30", amOrPM: "AM")
                     AlarmView(time: "8:15", amOrPM: "AM")
                     AlarmView(time: "9:00", amOrPM: "AM")
